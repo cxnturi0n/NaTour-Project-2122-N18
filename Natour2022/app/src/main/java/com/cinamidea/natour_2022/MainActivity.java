ec42e19a43ac9ac59639ec03178ee9a9760f3087
FabioCinicolo <f.cinicolo@studenti.unina.it>
2021-12-20 00:06:24 +0100

    Frontend update
    -Added network permission
    -Tried to invoke lambdas using Volley

--- Natour2022/app/src/main/java/com/cinamidea/natour_2022/MainActivity.java
+++ Natour2022/app/src/main/java/com/cinamidea/natour_2022/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("key", "signup");
                 buttonAnimation(button_signup);
 
+
             }
         });
 
--- Natour2022/app/src/main/java/com/cinamidea/natour_2022/SignupFragment.java
+++ Natour2022/app/src/main/java/com/cinamidea/natour_2022/SignupFragment.java
@@ -7,19 +7,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupFragment extends Fragment {
 
     private Button button;
     private Animation scale_up, scale_down;
     private Intent intent;
+    private EditText usernameET, emailET, passwordET;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,11 +52,16 @@ public class SignupFragment extends Fragment {
         button = view.findViewById(R.id.fragmentSignup_signup);
         scale_up = AnimationUtils.loadAnimation(getActivity(), R.anim.scale_up);
         scale_down = AnimationUtils.loadAnimation(getActivity(), R.anim.scale_down);
+        usernameET = view.findViewById(R.id.fragmentSignup_username);
+        emailET = view.findViewById(R.id.fragmentSignup_email);
+        passwordET = view.findViewById(R.id.fragmentSignup_password);
+
         intent = new Intent(getActivity(), PinActivity.class);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                handleSignUp();
                 buttonAnimation(button);
             }
         });
@@ -62,4 +84,33 @@ public class SignupFragment extends Fragment {
 
     }
 
+    private void handleSignUp() {
+
+        RequestQueue queue = Volley.newRequestQueue(this.getContext());
+        String username = usernameET.getText().toString();
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+        String url = "https://eagwqm6kz0.execute-api.eu-central-1.amazonaws.com/dev/user/"+username;
+        //BODY PER LA POST
+        //Map<String, String>params = new HashMap<>();
+        //params.put("username",username);
+        //params.put("email",email);
+        //params.put("password",password);
+        //JSONObject body = new JSONObject(params);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("OK",response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                            Log.e("ERROR1",error.getMessage());
+                    }
+                });
+        Volley.newRequestQueue(this.getContext()).add(jsonObjectRequest);
+    }
 }
