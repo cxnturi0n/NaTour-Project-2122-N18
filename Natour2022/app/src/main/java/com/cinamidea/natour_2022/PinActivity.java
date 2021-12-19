c439914e9fe40d1de7cfaa9249b911cf30fceadd
vonrisen <68206419+Vonrisen@users.noreply.github.com>
2021-12-19 18:19:19 +0100

    added intent to pinactivity

--- Natour2022/app/src/main/java/com/cinamidea/natour_2022/PinActivity.java
+++ Natour2022/app/src/main/java/com/cinamidea/natour_2022/PinActivity.java
@@ -2,7 +2,11 @@ package com.cinamidea.natour_2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class PinActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class PinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin);
+
     }
+
 }
